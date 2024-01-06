35c13ecbf07da9951aeaa0c6643e6d0d1ad5d8d2
Ayten Tuqusheva <aytentuqusheva@Aytens-MacBook-Pro.local>
2024-01-07 00:15:15 +0400

    Profile image animation added

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,9 +1,11 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:test_web/globals/app_assets.dart';
 import 'package:test_web/globals/app_colors.dart';
 import 'package:test_web/globals/app_text_styles.dart';
 import 'package:test_web/globals/constants.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:test_web/widgets/profile_animation.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -45,84 +47,109 @@ class HomePage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.only(
-            top: size.height * 0.3,
-            left: size.width * 0.2,
-            right: size.width * 0.2),
+            top: size.height * 0.05,
+            left: size.width * 0.1,
+            right: size.width * 0.1),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Hello, it\'s Me',
-                      style: AppTextStyles.monteseratStyle(color: Colors.white),
-                    ),
-                    Constants.sizedBox(height: 15),
-                    Text(
-                      'Ayten Huseynzade',
-                      style: AppTextStyles.headingtStyle(),
-                    ),
-                    Constants.sizedBox(height: 15),
-                    Row(
-                      children: [
-                        Text(
-                          'And I\'m a ',
-                          style: AppTextStyles.monteseratStyle(
-                              color: Colors.white),
-                        ),
-                        AnimatedTextKit(
-                          animatedTexts: [
-                            TyperAnimatedText('Flutter Developer',
-                                textStyle: AppTextStyles.monteseratStyle(
-                                    color: Colors.lightBlue)
-                                //speed: Duration(milliseconds: 400)
-                                ),
-                            TyperAnimatedText(
-                              '.NET Developer',
-                              textStyle: AppTextStyles.monteseratStyle(
-                                  color: Colors.lightBlue),
-                            ),
-                            TyperAnimatedText('Angular Developer',
-                                textStyle: AppTextStyles.monteseratStyle(
-                                    color: Colors.lightBlue)),
-                          ],
-                          pause: const Duration(milliseconds: 1000),
-                          displayFullTextOnTap: true,
-                          stopPauseOnTap: true,
-                        ),
-                      ],
-                    ),
-                    Constants.sizedBox(height: 15),
-                    SizedBox(
-                      width: size.width * 0.5,
+                    FadeInDown(
+                      duration: Duration(milliseconds: 1200),
                       child: Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                        ' Maecenas varius in odio nec condimentum. Nunc vel porta quam.'
-                        ' Cras sed pharetra enim, nec fermentum orci. '
-                        'Donec volutpat lobortis tortor quis pulvinar. Mauris nec congue nulla.',
-                        style: AppTextStyles.normalStyle(),
+                        'Hello, it\'s Me',
+                        style: AppTextStyles.monteseratStyle(color: Colors.white),
+                      ),
+                    ),
+                    Constants.sizedBox(height: 15),
+                    FadeInRight(
+                      duration: Duration(milliseconds: 1400),
+                      child: Text(
+                        'Ayten Huseynzade',
+                        style: AppTextStyles.headingtStyle(),
+                      ),
+                    ),
+                    Constants.sizedBox(height: 15),
+                    FadeInLeft(
+                      duration: Duration(milliseconds: 1400),
+                      child: Row(
+                        children: [
+                          Text(
+                            'And I\'m a ',
+                            style: AppTextStyles.monteseratStyle(
+                                color: Colors.white),
+                          ),
+                          AnimatedTextKit(
+                            animatedTexts: [
+                              TyperAnimatedText('Flutter Developer',
+                                  textStyle: AppTextStyles.monteseratStyle(
+                                      color: Colors.lightBlue)
+                                  //speed: Duration(milliseconds: 400)
+                                  ),
+                              TyperAnimatedText(
+                                '.NET Developer',
+                                textStyle: AppTextStyles.monteseratStyle(
+                                    color: Colors.lightBlue),
+                              ),
+                              TyperAnimatedText('Angular Developer',
+                                  textStyle: AppTextStyles.monteseratStyle(
+                                      color: Colors.lightBlue)),
+                            ],
+                            pause: const Duration(milliseconds: 1000),
+                            displayFullTextOnTap: true,
+                            stopPauseOnTap: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Constants.sizedBox(height: 15),
+                    FadeInDown(
+                      duration: Duration(milliseconds: 1600),
+                      child: SizedBox(
+                        width: size.width * 0.5,
+                        child: Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                          ' Maecenas varius in odio nec condimentum. Nunc vel porta quam.'
+                          ' Cras sed pharetra enim, nec fermentum orci. '
+                          'Donec volutpat lobortis tortor quis pulvinar. Mauris nec congue nulla.',
+                          style: AppTextStyles.normalStyle(),
+                        ),
                       ),
                     ),
                     Constants.sizedBox(height: 22),
-                    Row(
-                      children: [
-                        buildSocialButton(asset: AppAssets.facebook),
-                        Constants.sizedBox(width: 12),
-                        buildSocialButton(asset: AppAssets.github),
-                        Constants.sizedBox(width: 12),
-                        buildSocialButton(asset: AppAssets.insta),
-                        Constants.sizedBox(width: 12),
-                        buildSocialButton(asset: AppAssets.linkedIn),
-                        Constants.sizedBox(width: 12),
-                        buildSocialButton(asset: AppAssets.twitter)
-                      ],
+                    FadeInUp(
+                      duration: Duration(milliseconds: 1600),
+                      child: Row(
+                        children: [
+                          buildSocialButton(asset: AppAssets.facebook),
+                          Constants.sizedBox(width: 12),
+                          buildSocialButton(asset: AppAssets.github),
+                          Constants.sizedBox(width: 12),
+                          buildSocialButton(asset: AppAssets.insta),
+                          Constants.sizedBox(width: 12),
+                          buildSocialButton(asset: AppAssets.linkedIn),
+                          Constants.sizedBox(width: 12),
+                          buildSocialButton(asset: AppAssets.twitter)
+                        ],
+                      ),
                     ),
                     Constants.sizedBox(height: 18),
-                    buildMaterialButton(onTap: (){}),
+                    FadeInUp(
+                      duration: Duration(milliseconds: 1800),
+                      child: buildMaterialButton(onTap: (){}),)
+
                   ],
                 ),
+                //SizedBox(width: 20),
+                const ProfileAnimation(),
+
               ],
             ),
           ],
